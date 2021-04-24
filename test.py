dc0abe2b0e92e22b48c08ff61cdaf4bb9fc1674a
samuel-swinton <haroldham@gmail.com>
2021-04-24 15:35:08 -0400

    Adding sample pytest to test github actions

---
+++ test.py
@@ -0,0 +1,19 @@
+from beem import vote
+from datetime import datetime, timedelta
+import pytz
+
+
+def test_hivewatchers_votes():
+    yesterday = datetime.now() - timedelta(days=1)
+    tz = pytz.timezone('US/Eastern')
+    start_date = tz.localize(yesterday)
+    hivewatchers_votes = vote.AccountVotes('hivewatchers', start=start_date)
+    assert len(hivewatchers_votes) > 0, 'No hivewatchers votes'
+
+
+def test_fail_hivewatchers_votes():
+    yesterday = datetime.now() - timedelta(days=1)
+    tz = pytz.timezone('US/Eastern')
+    start_date = tz.localize(yesterday)
+    hivewatchers_votes = vote.AccountVotes('hivewatchers', start=start_date)
+    assert len(hivewatchers_votes) == 0, 'No hivewatchers votes'
